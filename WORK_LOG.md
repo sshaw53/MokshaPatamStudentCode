e1a4f636203cc30370cf347318f4935e9efef38e
sierrashaw <sierra.shaw@menloschool.org>
2024-09-05 20:02:29 -0700

    New try!!!

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,10 +1,12 @@
 Name: SIERRA SHAW
 
-| Date   |   Time    |                                                                                                                                                    Update |
-|:-------|:---------:|----------------------------------------------------------------------------------------------------------------------------------------------------------:|
-| Sept 2 | 12:20-1pm |                                                        I focused on writing out pseudocode to walk through my mental process of finding the fewest moves. |
-| Sept 2 | 1-1:30pm  | Realized my approach doesn't accurately determine the smallest number of moves, also trying to figure out how to access the ladders and snakes correctly. |
-| Sept 2 | 1:50-2:30 |                                                                                 Brainstorming a recursive approach - thinking broadly rather than coding. |
+| Date   |    Time    |                                                                                                                                                    Update |
+|:-------|:----------:|----------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Sept 2 | 12:20-1pm  |                                                        I focused on writing out pseudocode to walk through my mental process of finding the fewest moves. |
+| Sept 2 |  1-1:30pm  | Realized my approach doesn't accurately determine the smallest number of moves, also trying to figure out how to access the ladders and snakes correctly. |
+| Sept 2 | 1:50-2:30  |                                                                                 Brainstorming a recursive approach - thinking broadly rather than coding. |
+| Sept 4 | 10-10:45pm |                                                                   Read the reading passed out in class - didn't have time or energy to do any other work. |
+| Sept 5 |  2:30-3pm  |                                                                                                 Worked on implementing the new strategy for Moksha Patam. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
@@ -15,47 +18,44 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
-        // try to jump by 12 - avoid snakes - check if anything under 12 leads to a ladder
-        // if there's a ladder that gets you higher than
-        int board_side = (int) Math.sqrt(boardsize);
-        int i = 1;
-        int moves = 0;
-        boolean cont = true;
-        while (i < boardsize + 1) {
-            cont = true;
-            if (boardsize - i > 2 && boardsize - i < 12) {
-                return moves + 1;
-            }
-            // ?: EDGE CASE - 1 SPACE LEFT --> add 2 moves (can we automatically assume that though)
-            // Check if there's a ladder btwn i and i + 12 (including i + 12)
-            // ?: could be mult ladders
-            if (ladders[i][0] > i && ladders[i][0] <= i + 12) {
-                // ?: what if there's a ladder at i + 1 but we can't roll to it?
-                // If yes, check if the # at the end of the ladder (how to find that ?) is > i + 24
-                if (ladders[i][1] > i + 24) {
-                    // If yes, set i = end of ladder #, moves += 1, break out of if-statement loops to continue to while loop by setting
-                    // cont to false
-                    i = ladders[i][1];
-                    moves += 1;
-                    cont = false;
+        // Create an array of the snakes / ladders outputs for each input (index)
+        int[] snake_ladder = new int[boardsize + 1];
+        for (int i = 0; i < ladders.length; i++) {
+            snake_ladder[ladders[i][0]] = ladders[i][1];
+        }
+        for (int i = 0; i < snakes.length; i++) {
+            snake_ladder[snakes[i][0]] = snakes[i][1];
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        int current;
+        int[] rolls = new int[boardsize + 1];
+        int node;
+        boolean[] visited = new boolean[boardsize + 1];
+
+        // Add the first node to the queue
+        queue.add(1);
+        // While there are still nodes left in the queue
+        while (!queue.isEmpty()) {
+            // Remove it from the current queue and add it to the visited array
+            current = queue.remove();
+            visited[current] = true;
+            // If we've reached the end of the board, return the number of rolls
+            if (current >= boardsize - 6) { return rolls[current]; }
+            // Finding the possible future moves
+            for (int i = 1; i < 7; i++) {
+                node = current + i;
+                // Check to see if there's a snake or ladder to alter the possible nodes to visit
+                if (snake_ladder[node] != 0) {
+                    node = snake_ladder[node];
                 }
-            }
-            // If cont is true, check if there's a snake at i + 12
-            // If snake, check i + 11 & continue down until no snake
-            if (cont) {
-                int place = i + 12;
-                while (snakes[i][0] == place) {
-                    place -= 1;
+                // If the node hasn't been visited, add it to the back of the queue and save roll #
+                if (!visited[node]) {
+                    rolls[node] = rolls[current] + 1;
+                    queue.add(node);
                 }
-                // Set i = i + highest value w/o snake
-                i = place;
-                moves += 1;
             }
         }
-        // Does this use recursion??
-        // base case: being one roll away from board_size
-        // what's easiest / shortest to get between board_size and board_size-12 - for each value
-        //
-        return 0;
+        return -1;
     }
 }
